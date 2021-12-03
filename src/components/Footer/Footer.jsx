d890b3e172f5a7fbe61252bb01c906617b3d3697
Fernando Souza <carolina.anazago@gmail.com>
2021-12-03 00:52:11 +0000

    projeto-guiado

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { BsFillSuitHeartFill } from 'react-icons/bs'
+import './footer.styles.css'
+
+const Footer = () => {
+  return(
+    <div className="footer">
+      <span>Feito com <BsFillSuitHeartFill size={16} color="#fff"/> pela aluna Ana Carolina Zago - 2021</span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import './menu.styles.css'
+
+const Menu = () =>{
+  return(
+    <ul className="navbar">
+      <li>
+        <Link className="link" to="/">Home</Link>
+      </li>
+      <li>
+        <Link  className="link" to="/lista">Lista de Tarefas</Link>
+      </li>
+      <li>
+        <Link className="link" to="/maravilhosa">M de Maravilhosa</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Menu
